9db16d022ce453645b6977b90045f4550884a6bf
Tobias Preuss <tobias.preuss@googlemail.com>
2012-10-14 00:39:20 +0200

    Added buttons to check in and out.

--- src/info/metadude/android/shitappens/MainActivity.java
+++ src/info/metadude/android/shitappens/MainActivity.java
@@ -1,15 +1,26 @@
 package info.metadude.android.shitappens;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener {
+
+	protected Button loginButton;
+	protected Button logoutButton;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        loginButton = (Button)findViewById(R.id.loginButton);
+        logoutButton = (Button)findViewById(R.id.logoutButton);
+        loginButton.setOnClickListener(this);
+        logoutButton.setOnClickListener(this);
     }
 
     @Override
@@ -17,4 +28,13 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
     }
+
+	public void onClick(View view) {
+		if (view.getId() == loginButton.getId()) {
+			Toast.makeText(this, "Login Button clicked", Toast.LENGTH_LONG).show();
+		}
+		if (view.getId() == logoutButton.getId()) {
+			Toast.makeText(this, "Logout Button clicked", Toast.LENGTH_LONG).show();
+		}
+	}
 }
