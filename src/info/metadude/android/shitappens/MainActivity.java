22e047e29d56592668b4a5f2c71c2bd06a13bfb7
Tobias Preuss <tobias.preuss@googlemail.com>
2012-10-14 11:41:12 +0200

    Added ActionBarSherlock library to project.
    + Requires Java 1.6.
    + Android Support Libraries must be equal in both projects.

--- src/info/metadude/android/shitappens/MainActivity.java
+++ src/info/metadude/android/shitappens/MainActivity.java
@@ -1,32 +1,27 @@
 package info.metadude.android.shitappens;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity extends Activity implements OnClickListener {
+import com.actionbarsherlock.app.SherlockActivity;
+
+public class MainActivity extends SherlockActivity implements OnClickListener {
 
 	protected Button loginButton;
 	protected Button logoutButton;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+    	setTheme(R.style.Sherlock___Theme_DarkActionBar);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         loginButton = (Button)findViewById(R.id.loginButton);
         logoutButton = (Button)findViewById(R.id.logoutButton);
         loginButton.setOnClickListener(this);
         logoutButton.setOnClickListener(this);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
     }
 
 	public void onClick(View view) {
